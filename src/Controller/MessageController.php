9badd3f228ead9beaaf95da440e8fbaa320b904c
Kelyddon <kelyddon.diemunsch@gmail.com>
2025-12-31 11:27:35 +0100

    Page messages + manque le readme

---
+++ src/Controller/MessageController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\ContactMessage;
+use App\Form\ContactMessageType;
+use App\Repository\ContactMessageRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/message')]
+final class MessageController extends AbstractController
+{
+    #[Route(name: 'app_message_index', methods: ['GET'])]
+    public function index(ContactMessageRepository $contactMessageRepository): Response
+    {
+        return $this->render('message/index.html.twig', [
+            'contact_messages' => $contactMessageRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_message_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $contactMessage = new ContactMessage();
+        $form = $this->createForm(ContactMessageType::class, $contactMessage);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($contactMessage);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_message_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('message/new.html.twig', [
+            'contact_message' => $contactMessage,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_message_show', methods: ['GET'])]
+    public function show(ContactMessage $contactMessage): Response
+    {
+        return $this->render('message/show.html.twig', [
+            'contact_message' => $contactMessage,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_message_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, ContactMessage $contactMessage, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ContactMessageType::class, $contactMessage);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_message_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('message/edit.html.twig', [
+            'contact_message' => $contactMessage,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_message_delete', methods: ['POST'])]
+    public function delete(Request $request, ContactMessage $contactMessage, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$contactMessage->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($contactMessage);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_message_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
